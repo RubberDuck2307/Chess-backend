78d8595dfa874a140537eca5b5512b082f516664
Jan Metela <janmetela@seznam.cz>
2024-05-12 14:52:42 +0200

    continuous deployment

--- src/main/java/org/example/rest/GameRestController.java
+++ src/main/java/org/example/rest/GameRestController.java
@@ -4,10 +4,8 @@ import lombok.RequiredArgsConstructor;
 import org.example.rest.dto.game.NewGameResponseDto;
 import org.example.service.GameService;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
 import java.util.Set;
 import java.util.logging.Logger;
 
@@ -25,5 +23,10 @@ public class GameRestController {
         long id = gameService.createGame(Set.of(1L, 2L));
         logger.info("Game created with id " + id);
         return ResponseEntity.ok(new NewGameResponseDto(id));
+    }
+
+    @GetMapping("/")
+    public ResponseEntity<String> getGame() {
+        return ResponseEntity.ok("Hello World");
     }
 }
